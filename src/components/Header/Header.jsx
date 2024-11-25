744ad08fd9955b68584c3b8a5002ec946fa77150
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-25 16:00:23 -0700

    adds transitions to buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/Logo.svg";
 import "./Header.scss";
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    setTimeout(() => {
+      navigate(path);
+    }, 200);
+  };
   return (
     <nav className="nav">
       <Link to="/" className="nav__logo-link">
         <img src={logo} alt="teagens tutoring logo" className="nav__logo" />
       </Link>
       <div className="nav__button-container">
-        <Link to="/about">
-          <div className="nav__button">About</div>
-        </Link>
-        <Link to="/book">
-          <div className="nav__button">Book A Session</div>
-        </Link>
+        <div className="nav__button" onClick={() => handleClick("/about")}>
+          About
+        </div>
+
+        <div className="nav__button" onClick={() => handleClick("/book")}>
+          Book A Session
+        </div>
       </div>
     </nav>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import "./HomePage.scss";
+
 function HomePage() {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    setTimeout(() => {
+      navigate(path);
+    }, 200);
+  };
   return (
     <>
       <Header />
       <div className="main-container">
-        <Link to="/physics/topics" className="subject-link">
-          <div className="physics">physics</div>
-        </Link>
-        <Link to="/math/topics" className="subject-link">
-          <div className="math">math</div>
-        </Link>
+        <div className="physics" onClick={() => handleClick("/physics/topics")}>
+          physics
+        </div>
+        <div className="math" onClick={() => handleClick("/math/topics")}>
+          math
+        </div>
       </div>
     </>
   );
