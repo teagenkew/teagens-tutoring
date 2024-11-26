c8f1231042971ad3f90959f230887a9f99da843b
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-25 23:17:38 -0700

    adds some styling to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import physicsImage from "../../assets/physics-home.png";
+import mathImage from "../../assets/math-home.png";
+import physicsIcon from "../../assets/physics-icon.svg";
+import mathIcon from "../../assets/math-icon.svg";
 import Header from "../../components/Header/Header";
 import "./HomePage.scss";
 
@@ -15,10 +19,12 @@ function HomePage() {
       <Header />
       <div className="main-container">
         <div className="physics" onClick={() => handleClick("/physics/topics")}>
-          physics
+          <h3 className="card__title">physics</h3>
+          <div className="card__physics-icon"></div>
         </div>
         <div className="math" onClick={() => handleClick("/math/topics")}>
-          math
+          <h3 className="card__title">math</h3>
+          <div className="card__math-icon"></div>
         </div>
       </div>
     </>
