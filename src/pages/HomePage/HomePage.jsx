2a04208a0966d0b588c8c50656354b4de0b5541f
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-12-03 15:57:57 -0700

    adds navbar to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import NavBar from "../../components/NavBar/NavBar";
 import "./HomePage.scss";
 
 function HomePage() {
@@ -13,15 +14,26 @@ function HomePage() {
   return (
     <>
       <Header />
-      <div className="main-container">
-        <div className="physics" onClick={() => handleClick("/physics/topics")}>
-          <h3 className="card__title">physics</h3>
-          <div className="card__physics-icon"></div>
+      <div className="homepage">
+        <div className="homepage__nav-container">
+          <NavBar color={"pink"} />
         </div>
-        <div className="math" onClick={() => handleClick("/math/topics")}>
-          <h3 className="card__title">math</h3>
-          <div className="card__math-icon"></div>
+        <div className="main-container">
+          <div
+            className="physics"
+            onClick={() => handleClick("/physics/topics")}
+          >
+            <h3 className="card__title">physics</h3>
+            <div className="card__physics-icon"></div>
+          </div>
+          <div className="math" onClick={() => handleClick("/math/topics")}>
+            <h3 className="card__title">math</h3>
+            <div className="card__math-icon"></div>
+          </div>
         </div>
+        {/* <div className="homepage__about-container">
+          <About />
+        </div> */}
       </div>
     </>
   );
