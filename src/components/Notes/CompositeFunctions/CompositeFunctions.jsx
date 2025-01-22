4699220fc2da6a5e175cc24f956b99edfed77bea
teagenkew <96027598+teagenkew@users.noreply.github.com>
2025-01-21 20:08:31 -0700

    adds compositeFunctions files

---
+++ src/components/Notes/CompositeFunctions/CompositeFunctions.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import { InlineMath } from "react-katex";
+import "katex/dist/katex.min.css";
+import "./CompositeFunctions.scss";
+
+import React from 'react'
+
+function CompositeFunctions() {
+  return (
+    <div>CompositeFunctions</div>
+  )
+}
+
+export default CompositeFunctions
